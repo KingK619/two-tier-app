56a6e16e133f76c592da516814a41d38b0d819a9
Karanveer Sidana <bathinda880@gmail.com>
2026-04-22 20:10:50 +0530

    terraform-code

---
+++ main.tf
@@ -0,0 +1,156 @@
+# 1. Define the Cloud Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 2. Create the Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-enterprise-rg"
+  location = "East US" # Change to your preferred region
+}
+
+# 3. Create the Virtual Network and Subnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "devops-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "devops-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 4. Create the Public IP Address (Upgraded to Standard SKU)
+resource "azurerm_public_ip" "public_ip" {
+  name                = "devops-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"   # Changed from Dynamic
+  sku                 = "Standard" # Explicitly request a Standard IP
+}
+
+# 5. Create the Network Security Group (The Firewall)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "devops-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # Rule 1: SSH Access (Terminal)
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Rule 2: Jenkins CI/CD Engine
+  security_rule {
+    name                       = "Allow-Jenkins"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Rule 3: Flask Web Application
+  security_rule {
+    name                       = "Allow-FlaskApp"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Rule 4: Grafana Monitoring Dashboard
+  security_rule {
+    name                       = "Allow-Grafana"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  # Note: Prometheus (9090), Node Exporter (9100), and MySQL (3306) 
+  # do NOT need rules because they only talk internally on the Docker network!
+}
+
+# 6. Create the Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "devops-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Connect the NSG to the Network Interface
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# 7. Create the Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "devops-ubuntu-server"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  
+  # Standard_B2s is REQUIRED to run Jenkins and Docker simultaneously
+  size                  = "Standard_B2s" 
+
+  disable_password_authentication = false
+  admin_username                  = "azureuser"
+  admin_password                  = "EnterpriseDevOps2026!!" # Change to a secure password
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+# 8. Output the final Public IP to your terminal
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.vm.public_ip_address
+}
